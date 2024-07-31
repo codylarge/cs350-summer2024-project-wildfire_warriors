7cbf515e941c1f35e7f52f815f2738c5aa5dffe2
mafroese <mfroese1@ewu.edu>
2024-07-31 12:14:13 -0700

    progress
    progress

--- healthconnect/apps/pharmacists/apps.py
+++ healthconnect/apps/pharmacists/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PharmacistsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pharmacists'
+    name = 'apps.pharmacists'
