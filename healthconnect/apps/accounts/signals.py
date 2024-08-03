c6243e5865ce3860eaf976f8c696757e52087328
Cody Large <dlarge1@ewu.edu>
2024-08-02 21:14:40 -0700

    Register Logic
    Can now register and it will create the correct profile (patient/staff) and the user can view their info on their profile

---
+++ healthconnect/apps/accounts/signals.py
@@ -0,0 +1,14 @@
+# When a CustomUser instance is saved, the post_save signal is triggered. The create_user_profile function checks if the user was newly created (created is True) 
+# and then creates the appropriate profile based on the user's role.
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import CustomUser, PatientProfile, StaffProfile
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.is_patient:
+            PatientProfile.objects.create(user=instance)
+        elif instance.is_doctor or instance.is_nurse or instance.is_pharmacist:
+            StaffProfile.objects.create(user=instance)
