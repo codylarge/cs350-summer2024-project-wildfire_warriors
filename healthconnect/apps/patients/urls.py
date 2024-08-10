91775af2f59051182d7432670eb7ffb8584d1262
Cody Large <dlarge1@ewu.edu>
2024-08-10 11:28:46 -0700

    Backend Organization

--- healthconnect/apps/patients/urls.py
+++ healthconnect/apps/patients/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('medical-record/', views.medical_record, name='medical-record'),
-    path('book-appointment/', views.book_appointment, name='book-appointment'),
+    path('medical-record/', views.medical_record, name='medical_record'),
+    path('book-appointment/', views.book_appointment, name='book_appointment'),
+    path('medical-records/', views.view_medical_records, name='view_medical_records'),
+    path('medical-records/modify/<int:record_id>/', views.modify_medical_record, name='modify_medical_record'),
+    path('medical-records/modify/', views.modify_medical_record, name='add_medical_record'),
 ]
